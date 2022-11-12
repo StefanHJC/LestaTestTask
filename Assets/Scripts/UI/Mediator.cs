41bc522c021ac29bd9d01d99bf2d6e2d4a3118b4
Stefan <77760118+StefanHJC@users.noreply.github.com>
2022-11-13 01:04:05 +0300

    Update Mediator.cs

--- Assets/Scripts/UI/Mediator.cs
+++ Assets/Scripts/UI/Mediator.cs
@@ -5,12 +5,7 @@ namespace UI
     public class Mediator
     {
         [Inject] private BlockSelector _blockSelector;
-        //[Inject] private TimerBar _timerBar;
-        //[Inject] private ArrowIndicator _arrowIndicator;
-        //[Inject] private Menu _menu;
 
-       // public void ToggleMenu() => _menu.Toggle();
-       // public void CloseMenu() => _menu.Close();
         public void SelectBlock(Core.Blocks.MovableBlock block) => _blockSelector.Select(block);
         public void FocusBlock(Core.Blocks.MovableBlock block) => _blockSelector.Focus(block);
         public void DeselectBlock(Core.Blocks.MovableBlock block) => _blockSelector.Deselect(block);
